ca6e3d30098e63efa068dc8456543445b031971a
Junior <vilmarmjrr@gmail.com>
2020-12-17 19:44:01 -0300

    refactor: change validation to receive an object instead of a value

--- src/validation/validators/compare-fields/compare-fields-validation.ts
+++ src/validation/validators/compare-fields/compare-fields-validation.ts
@@ -2,9 +2,9 @@ import { InvalidFieldError } from '@/validation/errors'
 import { FieldValidation } from '@/validation/protocols/field-validation'
 
 export class CompareFieldsValidation implements FieldValidation {
-  constructor (readonly field: string, private readonly valueToCompare: string) {}
+  constructor (readonly field: string, private readonly fieldToCompare: string) {}
 
-  validate (value: string): Error {
-    return value !== this.valueToCompare ? new InvalidFieldError() : null
+  validate (input: object): Error {
+    return input[this.field] !== input[this.fieldToCompare] ? new InvalidFieldError() : null
   }
 }
