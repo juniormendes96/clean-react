fde7a20f81bc7525b56531958d312efe2e2fab3f
Junior <vilmarmjrr@gmail.com>
2020-12-22 19:32:57 -0300

    feat: add getCurrentAccountAdapter

--- src/main/adapters/current-account-adapter.ts
+++ src/main/adapters/current-account-adapter.ts
@@ -2,6 +2,10 @@ import { makeLocalStorageAdapter } from './../factories/cache/local-storage-adap
 import { UnexpectedError } from '@/domain/errors'
 import { AccountModel } from '@/domain/models'
 
+export const getCurrentAccountAdapter = (): AccountModel => {
+  return makeLocalStorageAdapter().get('account')
+}
+
 export const setCurrentAccountAdapter = (account: AccountModel): void => {
   if (!account?.accessToken) {
     throw new UnexpectedError()
