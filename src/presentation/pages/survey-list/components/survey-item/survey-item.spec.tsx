97c0b6fed97b0ea6becfc92403dd155b33505f92
Junior <vilmarmjrr@gmail.com>
2020-12-23 19:16:11 -0300

    refactor: move SUT creation to a factory method

--- src/presentation/pages/survey-list/components/survey-item/survey-item.spec.tsx
+++ src/presentation/pages/survey-list/components/survey-item/survey-item.spec.tsx
@@ -4,14 +4,21 @@ import { render, screen } from '@testing-library/react'
 import { SurveyItem } from '@/presentation/pages/survey-list/components'
 import { mockSurveyModel } from '@/domain/test'
 import { IconName } from '@/presentation/components'
+import { SurveyModel } from '@/domain/models'
+
+const makeSut = (survey = mockSurveyModel()): void => {
+  render(<SurveyItem survey={survey} />)
+}
 
 describe('SurveyItem Component', () => {
   test('Should render with correct values', () => {
-    const survey = mockSurveyModel()
-    survey.didAnswer = true
-    survey.date = new Date('2020-01-10T00:00:00')
+    const survey: SurveyModel = {
+      ...mockSurveyModel(),
+      didAnswer: true,
+      date: new Date('2020-01-10T00:00:00')
+    }
+    makeSut(survey)
 
-    render(<SurveyItem survey={survey} />)
     expect(screen.getByTestId('icon')).toHaveProperty('src', IconName.thumbUp)
     expect(screen.getByTestId('question')).toHaveTextContent(survey.question)
     expect(screen.getByTestId('day')).toHaveTextContent('10')
