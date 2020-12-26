2bb053af4abef55fbc0f82f8576bfbab0c1e34c2
Junior <vilmarmjrr@gmail.com>
2020-12-26 14:07:13 -0300

    refactor: add useLogout to remote duplicated code

--- src/presentation/hooks/use-error-handler.ts
+++ src/presentation/hooks/use-error-handler.ts
@@ -1,19 +1,15 @@
-import { useContext } from 'react'
-import { useHistory } from 'react-router-dom'
-import { ApiContext } from '@/presentation/contexts'
+import { useLogout } from '@/presentation/hooks'
 import { AccessDeniedError } from '@/domain/errors'
 
 type CallbackType = (error: Error) => void
 type ResultType = CallbackType
 
 export const useErrorHandler = (callback: CallbackType): ResultType => {
-  const { setCurrentAccount } = useContext(ApiContext)
-  const history = useHistory()
+  const logout = useLogout()
 
   return (error: Error): void => {
     if (error instanceof AccessDeniedError) {
-      setCurrentAccount(undefined)
-      history.replace('/login')
+      logout()
     } else {
       callback(error)
     }
