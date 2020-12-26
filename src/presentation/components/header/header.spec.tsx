d7ce6729324019673d2a4f095773c966fb0fe854
Junior <vilmarmjrr@gmail.com>
2020-12-26 13:10:51 -0300

    test: ensure logout calls setCurrentAccount with undefined

---
+++ src/presentation/components/header/header.spec.tsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import { Router } from 'react-router-dom'
+import { fireEvent, render, screen } from '@testing-library/react'
+import { Header } from '@/presentation/components'
+import { ApiContext } from '@/presentation/contexts'
+import { createMemoryHistory } from 'history'
+
+describe('Header Component', () => {
+  test('Should call setCurrentAccount with null value', () => {
+    const history = createMemoryHistory({ initialEntries: ['/'] })
+    const setCurrentAccountMock = jest.fn()
+    render(
+      <ApiContext.Provider value={{ setCurrentAccount: setCurrentAccountMock }}>
+        <Router history={history}>
+          <Header />
+        </Router>
+      </ApiContext.Provider>
+    )
+    fireEvent.click(screen.getByTestId('logout'))
+    expect(setCurrentAccountMock).toHaveBeenCalledWith(undefined)
+    expect(history.location.pathname).toBe('/login')
+  })
+})
