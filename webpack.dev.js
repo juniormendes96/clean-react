d40562b84adcb74776f58b069e6ce138e05cee45
Junior <vilmarmjrr@gmail.com>
2020-12-28 20:01:22 -0300

    chore: config travis ci to use cypress

--- webpack.dev.js
+++ webpack.dev.js
@@ -28,7 +28,8 @@ module.exports = merge(common, {
   devServer: {
     contentBase: './public',
     writeToDisk: true,
-    historyApiFallback: true
+    historyApiFallback: true,
+    port: 3000
   },
   plugins: [
     new DefinePlugin({
