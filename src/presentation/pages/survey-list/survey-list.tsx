e01872e855249995402835f428b311aed83a6c73
Junior <vilmarmjrr@gmail.com>
2020-12-30 20:52:45 -0300

    fix: move Error for its own component

--- src/presentation/pages/survey-list/survey-list.tsx
+++ src/presentation/pages/survey-list/survey-list.tsx
@@ -1,8 +1,8 @@
 import React, { useEffect, useState } from 'react'
 
 import Styles from './survey-list-styles.scss'
-import { Footer, Header } from '@/presentation/components'
-import { SurveyContext, Error, SurveyListItem } from '@/presentation/pages/survey-list/components'
+import { Footer, Header, Error } from '@/presentation/components'
+import { SurveyContext, SurveyListItem } from '@/presentation/pages/survey-list/components'
 import { LoadSurveyList } from '@/domain/usecases'
 import { useErrorHandler } from '@/presentation/hooks'
 
@@ -24,13 +24,18 @@ const SurveyList: React.FC<Props> = ({ loadSurveyList }: Props) => {
       .catch(handleError)
   }, [state.reload])
 
+  const reload = (): void => setState(old => ({ surveys: [], error: '', reload: !old.reload }))
+
   return (
     <div className={Styles.surveyListWrap}>
       <Header />
       <div className={Styles.contentWrap}>
         <h2>Enquetes</h2>
         <SurveyContext.Provider value={{ state, setState }}>
-          {state.error ? <Error /> : <SurveyListItem />}
+          {state.error
+            ? <Error error={state.error} reload={reload} />
+            : <SurveyListItem />
+          }
         </SurveyContext.Provider>
       </div>
       <Footer />
