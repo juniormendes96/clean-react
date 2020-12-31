53f0928cbccfffbb52d945372e8c83db86b17f0f
Junior <vilmarmjrr@gmail.com>
2020-12-31 17:13:45 -0300

    fix: move Calendar tests back to SurveyList

--- src/presentation/pages/survey-list/components/item/item.spec.tsx
+++ src/presentation/pages/survey-list/components/item/item.spec.tsx
@@ -14,22 +14,30 @@ describe('SurveyItem Component', () => {
   test('Should render with correct values', () => {
     const survey: LoadSurveyList.Model = {
       ...mockSurveyModel(),
-      didAnswer: true
+      didAnswer: true,
+      date: new Date('2020-01-10T00:00:00')
     }
     makeSut(survey)
 
     expect(screen.getByTestId('icon')).toHaveProperty('src', IconName.thumbUp)
     expect(screen.getByTestId('question')).toHaveTextContent(survey.question)
+    expect(screen.getByTestId('day')).toHaveTextContent('10')
+    expect(screen.getByTestId('month')).toHaveTextContent('jan')
+    expect(screen.getByTestId('year')).toHaveTextContent('2020')
   })
 
   test('Should render with correct values', () => {
     const survey: LoadSurveyList.Model = {
       ...mockSurveyModel(),
-      didAnswer: false
+      didAnswer: false,
+      date: new Date('2019-05-03T00:00:00')
     }
     makeSut(survey)
 
     expect(screen.getByTestId('icon')).toHaveProperty('src', IconName.thumbDown)
     expect(screen.getByTestId('question')).toHaveTextContent(survey.question)
+    expect(screen.getByTestId('day')).toHaveTextContent('03')
+    expect(screen.getByTestId('month')).toHaveTextContent('mai')
+    expect(screen.getByTestId('year')).toHaveTextContent('2019')
   })
 })
