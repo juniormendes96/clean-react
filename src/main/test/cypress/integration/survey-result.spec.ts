2def241f06bba09d5e7b950306b83916e0e38fb0
Junior <vilmarmjrr@gmail.com>
2021-01-02 15:17:30 -0300

    test: ensure SurveyResult presents correct data on success

--- src/main/test/cypress/integration/survey-result.spec.ts
+++ src/main/test/cypress/integration/survey-result.spec.ts
@@ -4,6 +4,7 @@ import * as Helper from '../utils/helpers'
 const path = /surveys/
 const mockUnexpectedError = (): void => Http.mockServerError(path, 'GET')
 const mockAccessDeniedError = (): void => Http.mockForbiddenError(path, 'GET')
+const mockSuccess = (): void => Http.mockOk(path, 'GET', 'survey-result')
 
 describe('SurveyResult', () => {
   beforeEach(() => {
@@ -26,5 +27,29 @@ describe('SurveyResult', () => {
     mockAccessDeniedError()
 
     Helper.testUrl('/login')
+  })
+
+  it('Should present survey result', () => {
+    cy.visit('/surveys/any_id')
+
+    mockSuccess()
+
+    cy.getByTestId('question').should('have.text', 'Question')
+
+    cy.getByTestId('day').should('have.text', '03')
+    cy.getByTestId('month').should('have.text', 'fev')
+    cy.getByTestId('year').should('have.text', '2018')
+
+    cy.get('li:nth-child(1)').then(li => {
+      assert.equal(li.find('[data-testid="answer"]').text(), 'any_answer')
+      assert.equal(li.find('[data-testid="percent"]').text(), '70%')
+      assert.equal(li.find('[data-testid="image"]').attr('src'), 'any_image')
+    })
+
+    cy.get('li:nth-child(2)').then(li => {
+      assert.equal(li.find('[data-testid="answer"]').text(), 'any_answer_2')
+      assert.equal(li.find('[data-testid="percent"]').text(), '30%')
+      assert.notExists(li.find('[data-testid="image"]'))
+    })
   })
 })
