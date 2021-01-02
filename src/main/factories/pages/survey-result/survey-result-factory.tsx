d3824924577449b48e3b66d74f1ef87217d2aec7
Junior <vilmarmjrr@gmail.com>
2021-01-02 14:13:32 -0300

    feat: add SurveyResult composition

---
+++ src/main/factories/pages/survey-result/survey-result-factory.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { useParams } from 'react-router-dom'
+import { SurveyResult } from '@/presentation/pages'
+import { makeRemoteLoadSurveyResult } from '@/main/factories/usecases'
+
+export const makeSurveyResult: React.FC = () => {
+  const { id } = useParams<any>()
+
+  return <SurveyResult loadSurveyResult={makeRemoteLoadSurveyResult(id)} />
+}
