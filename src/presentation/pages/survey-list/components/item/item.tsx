cb5bf9ea419b32975dc0c7f16358b2595e15b081
Junior <vilmarmjrr@gmail.com>
2021-01-02 14:19:40 -0300

    feat: ensure SurveyList redirects to SurveyResult

--- src/presentation/pages/survey-list/components/item/item.tsx
+++ src/presentation/pages/survey-list/components/item/item.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import Styles from './item-styles.scss'
 import { Calendar, Icon, IconName } from '@/presentation/components'
 import { LoadSurveyList } from '@/domain/usecases'
@@ -17,7 +18,9 @@ const SurveyItem: React.FC<Props> = ({ survey }: Props) => {
         <Calendar date={survey.date} className={Styles.calendarWrap} />
         <p data-testid="question">{survey.question}</p>
       </div>
-      <footer>Ver resultado</footer>
+      <footer>
+        <Link data-testid="link" to={`/surveys/${survey.id}`}>Ver resultado</Link>
+      </footer>
     </li>
   )
 }
