d845ee5739fafe2b68a3112b2cbd0275909a12b7
Junior <vilmarmjrr@gmail.com>
2021-01-02 14:03:54 -0300

    feat: ensure SurveyResult reloads on button click

--- src/presentation/pages/survey-result/survey-result.tsx
+++ src/presentation/pages/survey-result/survey-result.tsx
@@ -15,14 +15,17 @@ const SurveyResult: React.FC<Props> = ({ loadSurveyResult }: Props) => {
   const [state, setState] = useState({
     isLoading: false,
     error: '',
-    surveyResult: null as LoadSurveyResult.Model
+    surveyResult: null as LoadSurveyResult.Model,
+    reload: false
   })
 
   useEffect(() => {
     loadSurveyResult.load()
       .then(surveyResult => setState(old => ({ ...old, surveyResult })))
       .catch(handleError)
-  }, [])
+  }, [state.reload])
+
+  const reload = (): void => setState(old => ({ isLoading: false, surveyResult: null, error: '', reload: !old.reload }))
 
   return (
     <div className={Styles.surveyResultWrap}>
@@ -46,7 +49,7 @@ const SurveyResult: React.FC<Props> = ({ loadSurveyResult }: Props) => {
         </>
         }
         {state.isLoading && <Loading />}
-        {state.error && <Error error={state.error} reload={() => {}} />}
+        {state.error && <Error error={state.error} reload={reload} />}
       </div>
       <Footer />
     </div>
