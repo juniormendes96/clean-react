c8407a1097bcac17a0c5490801e201bba769788d
Junior <vilmarmjrr@gmail.com>
2021-01-03 14:54:48 -0300

    test: ensure SurveyResult will not present Loading on active answer click

--- src/presentation/pages/survey-result/survey-result.spec.tsx
+++ src/presentation/pages/survey-result/survey-result.spec.tsx
@@ -140,4 +140,16 @@ describe('SurveyResult Component', () => {
 
     expect(history.location.pathname).toBe('/')
   })
+
+  test('Should not present Loading on active answer click', async () => {
+    makeSut()
+
+    await waitFor(() => screen.getByTestId('survey-result'))
+
+    const answersWrap = screen.queryAllByTestId('answer-wrap')
+
+    fireEvent.click(answersWrap[0])
+
+    expect(screen.queryByTestId('loading')).not.toBeInTheDocument()
+  })
 })
